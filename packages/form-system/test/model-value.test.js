d23e7e668f95a52bc8912e7168d4e6d4da80b450
CubLion <alex.ghiu@ing.com>
2020-02-21 15:18:03 +0100

    chore: init dispatch count - inputs

--- packages/form-system/test/model-value.test.js
+++ packages/form-system/test/model-value.test.js
@@ -1,4 +1,4 @@
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, unsafeStatic } from '@open-wc/testing';
 import sinon from 'sinon';
 
 import '@lion/checkbox-group/lion-checkbox-group.js';
@@ -10,6 +10,14 @@ import '@lion/radio/lion-radio.js';
 import '@lion/select-rich/lion-select-rich.js';
 import '@lion/select-rich/lion-options.js';
 import '@lion/option/lion-option.js';
+
+import '@lion/input/lion-input.js';
+import '@lion/input-amount/lion-input-amount.js';
+import '@lion/input-date/lion-input-date.js';
+import '@lion/input-datepicker/lion-input-datepicker.js';
+import '@lion/input-email/lion-input-email.js';
+import '@lion/input-iban/lion-input-iban.js';
+import '@lion/input-range/lion-input-range.js';
 
 describe('model value', () => {
   describe('consistent init dispatch count', () => {
@@ -67,6 +75,24 @@ describe('model value', () => {
         </lion-select-rich>
       `);
       expect(eventSpy.callCount).to.equal(consistentCount);
+    });
+
+    [
+      'input',
+      'input-amount',
+      'input-date',
+      'input-datepicker',
+      'input-email',
+      'input-iban',
+      'input-range',
+    ].forEach(suffix => {
+      const tag = unsafeStatic(`lion-${suffix}`);
+      it(`should apply to ${suffix}`, async () => {
+        await fixture(html`
+          <${tag} @model-value-changed="${eventSpy}"></${tag}>
+        `);
+        expect(eventSpy.callCount).to.equal(consistentCount);
+      });
     });
   });
 });
