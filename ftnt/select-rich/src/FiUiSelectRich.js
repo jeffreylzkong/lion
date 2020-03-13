c9e7e476b4e4b6fff9a9122219cbc63f628f59ca
jasmine <JeffreyKong@LINGZHANG-KONGs-iMac.local>
2020-03-12 19:21:04 -0700

    select-rich style is good

--- ftnt/select-rich/src/FiUiSelectRich.js
+++ ftnt/select-rich/src/FiUiSelectRich.js
@@ -1,3 +1,18 @@
+import { css } from '../../../packages/core/index.js';
+
 import { LionSelectRich } from '../../../packages/select-rich/index.js';
 
-export class FiUiSelectRich extends LionSelectRich {}
+import '../fi-ui-select-invoker.js';
+
+export class FiUiSelectRich extends LionSelectRich {
+  static get styles() {
+    return css``;
+  }
+
+  get slots() {
+    return {
+      ...super.slots,
+      invoker: () => document.createElement('fi-ui-select-invoker'),
+    };
+  }
+}
